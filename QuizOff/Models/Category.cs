4d6c55996710b87f242bbdf742f1a8f24e42f5da
Lazar <lazar.stanojevic@pmf.edu.rs>
2018-08-05 21:14:38 +0200

    Logika inicijalizacije izbačena iz klase

--- QuizOff/Models/Category.cs
+++ QuizOff/Models/Category.cs
@@ -10,29 +10,20 @@ namespace QuizOff.Models
     public class Category
     {
 
-        public Category(string id)
-        {
-            using (var db = new DbHelper())
-            {
-
-                var result = db.SelectSingleRow(
-                    new string[] { "name", "description", "image_url" }, 
-                    "from category where idcategory = @id", 
-                    new Dictionary<string, string>() { ["@id"] = id.ToString() }
-                );
-
-                Id = id;
-                Name = result["name"].ToString();
-                Description = result["description"].ToString();
-                ImageUrl = result["image_url"].ToString();
-
-            }
-        }
-
         public string Id { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public string ImageUrl { get; private set; }
+
+        public Category(string id, string name, string description, string imageUrl)
+        {
+
+            Id = id;
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+
+        }
 
     }
 }
